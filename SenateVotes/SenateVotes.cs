8dfe4c5f24616fe123aa76382de5b83185cfbc20
Ahmed Musse <amusse001@gmail.com>
2019-04-24 23:51:15 -0700

    configured function to run every day at 7 PM EST

--- SenateVotes/SenateVotes.cs
+++ SenateVotes/SenateVotes.cs
@@ -1,6 +1,4 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace SenateVotes
@@ -8,9 +6,9 @@ namespace SenateVotes
     public static class SenateVotes
     {
         [FunctionName("SenateVotes")]
-        public static void Run([TimerTrigger("0 0 23 * * 1-5")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 0 19 * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"SenateVotes function executed");
         }
     }
 }
